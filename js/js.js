d4a364621367007c5bb9130af4fffb5da1db1080
Pili-Escobar <66434189+Pili-Escobar@users.noreply.github.com>
2020-10-12 23:59:54 -0600

    Menu_Responsive
    menu responsive terminado

---
+++ js/js.js
@@ -0,0 +1,22 @@
+/*PROGRAMACIÓN DEL MENU EN MODO MOBIL*/
+
+var iconoMenuMobil = document.getElementById("iconoMenuResponsive");
+  botonCerrar = document.getElementById("iconoMenuCerrar");
+
+var nav = document.getElementById("nav");
+  botonesNav = document.getElementById("ulNav");
+
+function desplegarMenuMobil() {
+  nav.classList.add("activarNav");
+  botonesNav.classList.add("activarBotones");
+  botonCerrar.classList.remove("none");
+}
+
+function cerrar() {
+  nav.classList.remove("activarNav");
+  botonesNav.classList.remove("activarBotones");
+  botonCerrar.classList.add("none");
+}
+
+iconoMenuMobil.addEventListener("click", desplegarMenuMobil, true);
+botonCerrar.addEventListener("click", cerrar, true);
